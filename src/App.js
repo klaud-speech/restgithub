e0130ad295ac748ea769835722b304b11dd9f3a5
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-07-06 14:52:30 +0900

    ag-grid 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [keyword, setKeyword] = useState('');
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch('https://api.github.com/search/repositories?q={keyword}')
+      .then(response=>response.json())
+      .then(data=>setData(data.items))
+      .catch( err=> console.error(err))    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={keyword}
+        onChange={( e=>setKeyword(e.target.value))} />
+      <button onClick={fetchData}>Fetch</button>
+
+      <table>
+        <tbody>
+          {
+            data.map(repo=>
+              <tr>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
