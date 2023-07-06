5d94542e1dddb4866c191702c9cab4622b7f4740
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-07-06 15:07:06 +0900

    import & fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React, {useState} from 'react';
 
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+
 function App() {
   const [keyword, setKeyword] = useState('');
   const [data, setData] = useState([]);
@@ -12,11 +16,24 @@ function App() {
       .catch( err=> console.error(err))    
   }
 
+  const columns = [
+    {field: 'full_name'},
+    {field: 'html_url'},
+    {field: 'owner.login'}
+  ]
+
   return (
     <div className="App">
       <input value={keyword}
         onChange={( e=>setKeyword(e.target.value))} />
       <button onClick={fetchData}>Fetch</button>
+
+      <div className="ag-theme-material" style={ {height: 500 , width: '90%'} }>
+        <AgGridReact
+          rowData={data}
+          columnDefs={columns}
+        />
+      </div>
 
     </div>
   );
